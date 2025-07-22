3f1424cb563f100faf57577a505cbbde176eb393
goodlifechris <christopherndugo@gmail.com>
2025-07-22 17:37:26 +0400

    set up for api

--- database/migrations/2025_07_22_100807_create_tasks_table.php
+++ database/migrations/2025_07_22_100807_create_tasks_table.php
@@ -9,14 +9,16 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->boolean('completed')->default(false);
             $table->timestamps();
         });
     }
-
     /**
      * Reverse the migrations.
      */
